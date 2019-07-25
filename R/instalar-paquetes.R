fe9b0058daa6419c286d8367faff86959250f1cb
Edgar Ruiz <edgar@rstudio.com>
2019-07-25 14:45:42 -0500

    Fixes #2

--- R/instalar-paquetes.R
+++ R/instalar-paquetes.R
@@ -7,27 +7,32 @@ check_for <- c(
   "nasaweather", "Lahman", "gapminder", "xaringan",
   "flexdashboard", "leaflet", "janitor"
 )
-
-packages <- tools::CRAN_package_db()
 installed_packages <- as.data.frame(installed.packages())
-check_package <- function(x) {
-  cran <- packages[packages$Package == x, "Version"]
-  local <- as.character(installed_packages[installed_packages$Package == x, "Version"])
-  install <- FALSE
-  if(length(local) == 0) {
-    install = TRUE
-  } else {
-    if(local != cran) install = TRUE
+check_installed <- installed_packages[installed_packages$Package %in% check_for, ]
+check_installed$version <- gsub("\\.", "", check_installed$Version)
+check_installed$version <- gsub("\\-", "", check_installed$version)
+check_installed$version <- as.numeric(check_installed$version)
+cran_versions <- read.csv("R/versions.csv")
+compare_vers <- sapply(
+  check_for,
+  function(x) {
+    local_version <- check_installed[check_installed$Package == x, "version"]
+    cran_version <- cran_versions[cran_versions$Package == x, "version"]
+    cran_version > local_version
   }
-  list(
-    cran = cran,
-    local = local,
-    install = install
-  )
+)
+compare_vers <- as.vector(compare_vers)
+if(any(compare_vers)) {
+  install_pkgs <- check_for[compare_vers]
+  install.packages(install_pkgs)
 }
-check_versions <- lapply(check_for, check_package)
-check_versions <- setNames(check_versions, check_for)
-to_install <- as.vector(sapply(check_versions, function(x) x$install))
-update_packages <- names(check_versions[to_install])
-install.packages(update_packages)
-rm(check_versions, installed_packages, packages, check_for, to_install, update_packages, check_package)
+cran_versions <- function() {
+  packages <- tools::CRAN_package_db()
+  check_installed <- packages[packages$Package %in% check_for, ]
+  check_installed$version <- gsub("\\.", "", check_installed$Version)
+  check_installed$version <- gsub("\\-", "", check_installed$version)
+  check_installed$version <- as.numeric(check_installed$version)
+  write.csv(check_installed[ , c("Package", "Version", "version")], "R/versions.csv")
+}
+rm(check_installed, installed_packages, check_for, compare_vers, cran_versions)
+
