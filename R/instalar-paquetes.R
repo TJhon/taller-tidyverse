fe5f0decb11d910165772b687171b39cbf563ffe
Edgar Ruiz <edgar@rstudio.com>
2019-07-28 16:50:07 -0500

    tidytext slides

--- R/instalar-paquetes.R
+++ R/instalar-paquetes.R
@@ -5,7 +5,7 @@ check_for <- c(
   "mime", "tidyverse", "tidymodels", "fs", "skimr",
   "corrr", "babynames", "fueleconomy", "nycflights13",
   "nasaweather", "Lahman", "gapminder", "xaringan",
-  "flexdashboard", "leaflet", "janitor", "tidytext"
+  "flexdashboard", "leaflet", "janitor", "tidytext", "fs"
 )
 installed_packages <- as.data.frame(installed.packages())
 check_installed <- installed_packages[installed_packages$Package %in% check_for, ]
